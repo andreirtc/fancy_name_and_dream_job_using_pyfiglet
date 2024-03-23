c776cb1ddc05f2b92bc6767fef086914b4e9287e
Ralph Andrei Castillo <ralphandreicastillo326@gmail.com>
2024-03-23 19:33:38 +0800

    Display result

--- fancy_name_and_dream_job.py
+++ fancy_name_and_dream_job.py
@@ -12,8 +12,14 @@ from colorama import Fore, Style
 
 # 4. Ask for name (input)
 name = input("Enter your name: ")
+
 # 5. Ask for dream job (input)
 dream_job = input("Enter your dream job: ")
+
 # 6. Ask when to expect to graduate (in years)
-expected_graduation = input("When do you expect to graduate? (e.g. in 3 years): ")
+expected_graduation = input("When do you expect to graduate? (enter number of years only): ")
+
 # 7. Display name, dream job, and message in a fancy way
+figlet = Figlet(font = '3d-ascii')
+result = (f"{name}, YOU WILL BE AN AMAZING {dream_job} IN {expected_graduation} YEARS! AIM HIGH AND REACH THAT DREAM!")
+print(f"\n{Fore.CYAN}{Style.BRIGHT}{figlet.renderText(result)}")
