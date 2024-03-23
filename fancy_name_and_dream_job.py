2bf8481dfcefcbab11651a919304e0840c9768f5
Ralph Andrei Castillo <ralphandreicastillo326@gmail.com>
2024-03-23 19:18:38 +0800

    Import Figlet for fonts and import Fore, Style, for text color

--- fancy_name_and_dream_job.py
+++ fancy_name_and_dream_job.py
@@ -2,12 +2,18 @@
 # 1. Install pyfiglet and colorama module
     # pip install pyfiglet
     # pip install colorama
-# 2. Import pyfiglet (for fonts)
-    import pyfiglet
-# 3. Import colorama (for color and style)
-    import colorama
-# 4. From pyfiglet import figlet and from colorama import foreground and style
-# 5. Ask for name (input)
-# 6. Ask for dream job (input)
-# 7. Ask when to expect to graduate (in years)
-# 8. Display name, dream job, and message in a fancy way
+
+# 2. Import pyfiglet (for fonts) and colorama (for color and style)
+# 3. From pyfiglet import figlet and from colorama import foreground and style
+import pyfiglet
+from pyfiglet import Figlet
+import colorama
+from colorama import Fore, Style
+
+# 4. Ask for name (input)
+
+# 5. Ask for dream job (input)
+
+# 6. Ask when to expect to graduate (in years)
+
+# 7. Display name, dream job, and message in a fancy way
