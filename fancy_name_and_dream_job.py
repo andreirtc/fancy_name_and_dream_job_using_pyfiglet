54eebec65d1d9cb56c2379506337f6afa08f11a3
Ralph Andrei Castillo <ralphandreicastillo326@gmail.com>
2024-03-23 19:12:09 +0800

    Importing modules

--- fancy_name_and_dream_job.py
+++ fancy_name_and_dream_job.py
@@ -3,7 +3,9 @@
     # pip install pyfiglet
     # pip install colorama
 # 2. Import pyfiglet (for fonts)
+    import pyfiglet
 # 3. Import colorama (for color and style)
+    import colorama
 # 4. From pyfiglet import figlet and from colorama import foreground and style
 # 5. Ask for name (input)
 # 6. Ask for dream job (input)
