4b8a16c2f710395dd1973dfc7bd86847ef70b2c9
Ralph Andrei Castillo <ralphandreicastillo326@gmail.com>
2024-03-23 18:44:56 +0800

    Asking for name of the user

--- fancy_name_and_dream_job.py
+++ fancy_name_and_dream_job.py
@@ -1,5 +1,6 @@
 # Pseudocode
 # 1. Ask for name (input)
+name = input("Enter your name: ")
 # 2. Ask for dream job (input)
 # 3. Ask when to expect to graduate (in years)
 # 4. Display name, dream job, and message in a fancy way
