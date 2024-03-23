50eb792c92cca7a92e37e3b1ebbafcf70f1088ff
Ralph Andrei Castillo <ralphandreicastillo326@gmail.com>
2024-03-23 18:53:57 +0800

    Pseudocode revised

--- fancy_name_and_dream_job.py
+++ fancy_name_and_dream_job.py
@@ -1,12 +1,11 @@
 # Pseudocode
 # 1. Install pyfiglet and colorama module
     # pip install pyfiglet
-    # pip install colorama\
+    # pip install colorama
 # 2. Import pyfiglet (for fonts)
 # 3. Import colorama (for color and style)
 # 4. From pyfiglet import figlet and from colorama import foreground and style
 # 5. Ask for name (input)
 # 6. Ask for dream job (input)
 # 7. Ask when to expect to graduate (in years)
-# 8. 
-# 9. Display name, dream job, and message in a fancy way
+# 8. Display name, dream job, and message in a fancy way
